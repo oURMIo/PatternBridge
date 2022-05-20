2500b1fdc8d784a8d3632b51de2da8fbc7c01dcb
oURMIo <dimach.98.ru@gmail.com>
2022-05-20 18:33:17 +0300

    bridge

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,31 +1,20 @@
-public class Animal {
-    private AnimalType animalType;
+public record Animal(AnimalType animalType) {
 
-    private String name;
-    private boolean act;
-    private double meal;
-
-    public Animal(AnimalType animalType) {
-        this.name = animalType.getType();
-        this.act = animalType.isActive();
-        this.meal = animalType.getKgMeat();
-    }
-
-    public void feed(){
-        System.out.printf("give %s kg meal %n",meal);
+    public void feed() {
+        System.out.printf("give %s kg meal %n", animalType.getKgMeat());
     }
 
     public void send() {
-        if (act) {
+        if (animalType.isActive()) {
             System.out.printf(" Send this animal in village %n %n");
         } else {
             System.out.printf(" Send this animal in home %n %n");
         }
     }
 
-    public void print(){
-        System.out.printf(" It's a %s and it needs %s kg meal %n",name,meal);
-        if (act){
+    public void print() {
+        System.out.printf(" It's a %s and it needs %s kg meal %n", animalType.getType(), animalType.getKgMeat());
+        if (animalType.isActive()) {
             System.out.printf(" It's like play with u) %n");
         } else {
             System.out.printf(" It's like sit on your knees %n");
